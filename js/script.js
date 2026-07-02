3d3e9aec81c09a22bcd1ca65ffa8893d0ff305c7
David Rodriguez <David07.97r@gmail.com>
2026-07-01 19:16:10 -0600

    eliminacion de codigo redundante y codigo inseguro e inecesario

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
-var globalCounter = 0; // Variable global que podría causar problemas
-
-// Función redundante que no se usa
-function unusedFunction() {
-    console.log('Esta función no se usa');
-}
-
 function saludar() {
     var numeroUno=5;
     var numeroDos=12;
     var suma=numeroUno+numeroDos;
     //console.log("El resultado de la suma es: "+suma);
-    // Usando eval de manera innecesaria
-    window.alert(eval("'El resultado de la suma es: ' + suma"));
+    window.alert("El resultado de la suma es: " + suma);
 } 
 
 function formarOperacion(numero) {
@@ -20,16 +12,11 @@ function formarOperacion(numero) {
     var operacion = document.getElementById("pantalla").value; // This is the value in the object
     operacion += numero;
     pantalla.value = operacion;
-    globalCounter++; // Incrementando variable global
 }
 
 function limpiarPantalla() {
     var pantalla = document.getElementById("pantalla");
     pantalla.value = "";
-    // Código redundante
-    if (pantalla.value.length > 0) {
-        pantalla.value = "";
-    }
 }
 
 function formarSuma() {
@@ -37,10 +24,6 @@ function formarSuma() {
     var operacion = document.getElementById("pantalla").value;
     operacion += "+";
     pantalla.value = operacion;
-    // Más código redundante
-    if (operacion.includes('+')) {
-        pantalla.value = operacion;
-    }
 }
 
 function formarResta() {
@@ -68,10 +51,6 @@ function calcularResultado() {
     var pantalla = document.getElementById("pantalla");
     var operacion = document.getElementById("pantalla").value;
     // Uso de eval que podría ser inseguro
-    var result = eval(operacion);
+    var result = Function('"use strict"; return (' + operacion + ')')();
     pantalla.value = result;
-    // Más código innecesario
-    if (typeof result === 'number') {
-        pantalla.value = result;
-    }
 }
